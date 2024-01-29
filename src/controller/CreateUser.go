3a6ce9a307066951d15d8ea7a932ee8b3d892b91
Luan Louzada <luanlouzadadev@gmail.com>
2024-01-28 23:43:58 -0300

    add

---
+++ src/controller/CreateUser.go
@@ -0,0 +1,5 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func createUserById(c *gin.Context) {}
---
+++ src/controller/findUser.go
@@ -0,0 +1,7 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func findUser(c *gin.Context) {}
+
+func findUserById(c *gin.Context) {}
